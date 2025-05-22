bf066ecc67d29ba355332a4a9d2ac7bc6dd796dc
zeina mkhaeel <127236091+zeinamkhaeel@users.noreply.github.com>
2025-05-22 19:18:08 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,28 @@
 import streamlit as st
 
-st.markdown(
-    """
-    <div style="
-        position: fixed;
-        top: 0;
-        left: 0;
-        height: 100%;
-        width: 100%;
-        background-image: url(\\"https://images.unsplash.com/photo-1604917877931-84c43b1a1e5f?auto=format&fit=crop&w=1500&q=80\\");
-        background-size: cover;
-        background-position: center;
-        opacity: 0.15;
-        z-index: -1;
-    "></div>
-    """,
-    unsafe_allow_html=True
-)
+import streamlit as st
 
+def set_bg_image():
+    st.markdown(
+        """
+        <div style="
+            position: fixed;
+            top: 0;
+            left: 0;
+            height: 100%;
+            width: 100%;
+            background-image: url('https://images.unsplash.com/photo-1604917877931-84c43b1a1e5f?auto=format&fit=crop&w=1500&q=80');
+            background-size: cover;
+            background-position: center;
+            opacity: 0.15;
+            z-index: -1;
+        "></div>
+        """,
+        unsafe_allow_html=True
+    )
 
-set_bg_image()
+set_bg_image()  # <- this must come after the function
+
 
 import joblib
 import numpy as np
